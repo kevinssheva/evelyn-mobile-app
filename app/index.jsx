e84dcb714a4d178d3911265f40825efa9a1b1b32
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-05-29 15:13:02 +0700

    feat: protect all pages using global context and auth things

--- app/index.jsx
+++ app/index.jsx
@@ -1,30 +1,19 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
-import { Link } from "expo-router";
-import { onAuthStateChanged } from "firebase/auth";
-import { FIREBASE_AUTH } from "../config/FirebaseConfig";
-import { useRouter } from "expo-router";
+import { View } from "react-native";
+import React from "react";
+import { Link, Redirect } from "expo-router";
+import { useGlobalContext } from "../context/GlobalProvider";
 
 const App = () => {
-  const router = useRouter();
+  const { isLogged } = useGlobalContext();
 
-  useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      if (user) {
-        console.log("User is signed in");
-        console.log("user", user);
-        router.push("/home");
-      }
-    });
-  }, []);
+  if (isLogged) {
+    return <Redirect href="/home" />;
+  }
 
   return (
     <View className="justify-center items-center h-full">
       <Link href="/sign-in" className="font-ibold text-xl mb-4 underline">
         Go To Auth
-      </Link>
-      <Link href="/home" className="font-ibold text-xl mb-4 underline">
-        Go To Home
       </Link>
     </View>
   );
